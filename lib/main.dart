76044085bc16b2c1b6c1d8eb1adcc83155e2cf2d
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-07-01 16:16:03 +0900

    :sparkles: Add firebase init

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,26 @@
+import 'dart:async';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:simple_logger/simple_logger.dart';
 import 'package:todo/src/app/app.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  await FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(kDebugMode);
+  FlutterError.onError = (errDetails) async
+      => await FirebaseCrashlytics.instance.recordFlutterError(errDetails);
+
   SimpleLogger logger = SimpleLogger();
   String FLAVOR = String.fromEnvironment('FLAVOR', defaultValue: "production");
   logger.info(FLAVOR);
-  runApp(App());
+
+  runZonedGuarded(
+    () => runApp(ProviderScope(child: App())),
+    (err, stack) => FirebaseCrashlytics.instance.recordError(err, stack)
+  );
 }
