0e1168a08cf7c19a3098ed3037b93c79637e8b30
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-07-04 12:34:09 +0900

    :sparkles: Implement google sign in

--- lib/src/data/usecase/impl/sign_in_usecase_impl.dart
+++ lib/src/data/usecase/impl/sign_in_usecase_impl.dart
@@ -1,10 +1,13 @@
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo/src/data/model/user/user_model.dart';
+import 'package:todo/src/data/provider/firebase_auth_repository_provider.dart';
+import 'package:todo/src/data/repository/firebase_auth_repository.dart';
 import 'package:todo/src/data/usecase/sign_in_usecase.dart';
 
 class SignInUseCaseImpl implements SignInUseCase {
   SignInUseCaseImpl(this._reader);
   final Reader _reader;
+  late final FirebaseAuthRepository _authRepo = _reader(firebaseAuthRepositoryProvider);
 
   @override
   Future<UserModel?> executeWithGitHub() async {
@@ -13,8 +16,5 @@ class SignInUseCaseImpl implements SignInUseCase {
   }
 
   @override
-  Future<UserModel?> executeWithGoogle() async {
-    // TODO: implement executeWithGoogle
-    throw UnimplementedError();
-  }
+  Future<UserModel?> executeWithGoogle() async => _authRepo.signInWithGoogle();
 }
