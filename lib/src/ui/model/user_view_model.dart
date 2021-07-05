35b638ab5673fac487e25af51479f589e4fe56b7
Kotokaze <62094392+Kotokaze@users.noreply.github.com>
2021-07-05 10:53:16 +0900

    :sparkles: Add sign out usecase

--- lib/src/ui/model/user_view_model.dart
+++ lib/src/ui/model/user_view_model.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:todo/src/data/model/user/user_model.dart';
 import 'package:todo/src/data/provider/sign_in_usecase_provider.dart';
+import 'package:todo/src/data/provider/sign_out_usecase_provider.dart';
 import 'package:todo/src/data/usecase/sign_in_usecase.dart';
+import 'package:todo/src/data/usecase/sign_out_usecase.dart';
 
 final userViewModelProvider = ChangeNotifierProvider<UserViewModel>((ref) {
     return UserViewModel(ref.read);
@@ -12,6 +14,7 @@ class UserViewModel extends ChangeNotifier {
   UserViewModel(this._reader);
   final Reader _reader;
   late final SignInUseCase _signInUseCase = _reader(signInUseCaseProvider);
+  late final SignOutUseCase _signOutUseCase = _reader(signOutUseCaseProvider);
   UserModel? _user;
 
   UserModel? get user => _user;
@@ -23,4 +26,6 @@ class UserViewModel extends ChangeNotifier {
           _user = user;
           notifyListeners();
       }});
+
+  Future<void> handleSignOut() => _signOutUseCase.execute();
 }
